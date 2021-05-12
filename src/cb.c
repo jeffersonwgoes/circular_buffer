41cc8e6c801f38543780145d9109f3f8b1d6a942
jeffersonwgoes <jeffersonwgoeS@gmail.com>
2021-05-12 16:06:37 -0300

    Basic functionalities and happy path unit tests

--- src/cb.c
+++ src/cb.c
@@ -1,20 +1,35 @@
 #include "cb.h"
 
-bool cb_init(cb_t *cb)
+bool cb_init(cb_t *cb, uint8_t size)
 {
 	cb->start = 0;
 	cb->end = 0;
+	cb->size = size;
 	cb->count = 0;
 	return true;
 }
 
 bool cb_is_full(cb_t cb)
 {
-	return cb.start == cb.end && cb.count > 0;
+	return cb.count == cb.size;
 }
-
-
+	
 bool cb_is_empty(cb_t cb) 
 {
 	return cb.count == 0;
+}
+
+void cb_add(cb_t * cb, uint8_t item)
+{
+	cb->values[cb->start++] = item;
+	cb->start = cb->start == cb->size ? 0 : cb->start;
+	cb->count++;
+}
+	
+uint8_t cb_get(cb_t *cb)
+{
+	uint8_t item = cb->values[cb->end++];	
+	cb->end = cb->end == cb->size ? 0 : cb->end;
+	cb->count--;
+	return item;
 }
